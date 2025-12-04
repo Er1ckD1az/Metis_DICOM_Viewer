14176dbad33717c743b29d289bc145bb3b6e6438
ImmortalXXAlpha <john.montz@selu.edu>
2025-12-03 20:04:23 -0600

    Updated About Page

--- Projects/Metis_Capstone/frontend/src/App.tsx
+++ Projects/Metis_Capstone/frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import DicomViewer from "./components/DicomViewer";
+import About from "./pages/About";    
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/viewer" element={<DicomViewer />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   );
 }
